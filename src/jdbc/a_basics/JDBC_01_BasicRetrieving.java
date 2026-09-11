f282036864318c9b00c3069469705a1be1dba223
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-09-11 21:30:57 +0530

    Added JDBC_01_BasicRetrieving.java File.

---
+++ src/jdbc/a_basics/JDBC_01_BasicRetrieving.java
@@ -0,0 +1,38 @@
+/// Telusko -> 23:12:00 - 1:00:34:00
+/// -> 23:55:00
+
+package jdbc.a_basics;
+
+/*
+=========================================================
+                JDBC BASIC PROGRAM
+---------------------------------------------------------
+Features Covered:
+1. Load JDBC Driver
+2. Establish Database Connection
+3. Create Statement
+4. Execute SQL Query
+5. Process ResultSet
+6. Insert Data
+7. Close JDBC Resources
+=========================================================
+*/
+
+/**
+1. Connect IDE with the Database
+   -> Add MySQL Connector/J JAR file in External Libraries.
+   Example -> mysql-connector-j-9.7.0.jar
+*/
+
+/********************************************* 2. Import SQL Package ********************************************/
+
+import java.sql.*;
+
+public class JDBC_01_BasicRetrieving {
+
+    public static void main(String[] args) throws Exception {
+
+///        code here
+
+    }
+}
