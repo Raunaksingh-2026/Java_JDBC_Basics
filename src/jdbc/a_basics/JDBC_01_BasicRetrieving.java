ca0d1a37177525d799b892e4d6ebc85dc03af21c
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-09-12 20:59:30 +0530

    Updated JDBC_01_BasicRetrieving.java.

--- src/jdbc/a_basics/JDBC_01_BasicRetrieving.java
+++ src/jdbc/a_basics/JDBC_01_BasicRetrieving.java
@@ -17,7 +17,6 @@ Features Covered:
 7. Close JDBC Resources
 =========================================================
 */
-
 /**
 1. Connect IDE with the Database
    -> Add MySQL Connector/J JAR file in External Libraries.
@@ -29,10 +28,29 @@ Features Covered:
 import java.sql.*;
 
 public class JDBC_01_BasicRetrieving {
-
     public static void main(String[] args) throws Exception {
+/*
+        JDBC URL Format -> jdbc:mysql://host:port/database_name
+        Example -> jdbc:mysql://localhost:3306/sqldatabase1
+*/
+        String url = "jdbc:mysql://localhost:3306/sqldatabase1";
 
-///        code here
+        String userName = "javaUser";
+        String password = "Password";
+
+//      ============================== SQL Queries ============================
+
+//         Fetch selected columns from users table
+        String query1 = "SELECT name, date_of_birth FROM users WHERE id >= 2";
+
+//         Fetch all columns from users table
+        String query2 = "SELECT * FROM users";
+
+//         Insert a new user record into database
+        String query3 = "INSERT INTO users " +
+                        "(name, email, gender, date_of_birth) " +
+                        "VALUES " +
+                        "('Om Singh', 'om123@gmail.com', 'Male', '2007-04-29')";
 
     }
 }
